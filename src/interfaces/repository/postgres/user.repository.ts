be356855d8889b655236d9e295450c002bdb22f9
Daniil <d.stepanenko@noisyminer.com>
2020-10-09 14:13:17 +0300

    removed unused variable

--- src/interfaces/repository/postgres/user.repository.ts
+++ src/interfaces/repository/postgres/user.repository.ts
@@ -1,5 +1,5 @@
 import { User } from '../../../models/user.entity';
-import { EntityRepository, Repository } from 'typeorm/index';
+import { EntityRepository, Repository } from 'typeorm';
 import { AbstractUserRepository } from '../user.repository';
 import { Optional } from '../../../types';
 
@@ -17,7 +17,7 @@ export class PostgresUserRepository
   }
 
   async updateUser(id: number, user: Partial<User>): Promise<Optional<User>> {
-    const result = await this.update(id, user);
+    await this.update(id, user);
     return this.findById(id);
   }
 }
